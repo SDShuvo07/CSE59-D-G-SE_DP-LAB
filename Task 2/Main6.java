c8a8e906aa397547a211a1970ed2ebba3612f691
SDShuvo07 <ssd01733@gmail.com>
2026-06-18 17:04:00 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,20 @@
+class Applet {
+    void launch() {
+        System.out.println("applet launched");
+    }
+}
+
+class GameApplet extends Applet{
+    void play(){
+        System.out.println("playing game");
+    }
+}
+
+public class main6 {
+    public static void main(String[] arrgs){
+        GameApplet game = new GameApplet();
+        game.launch();
+        game.play();
+    }
+}
 

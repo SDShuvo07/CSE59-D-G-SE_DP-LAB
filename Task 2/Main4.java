9e3f167fe1c36ae475487f016414ff5074960b26
SDShuvo07 <ssd01733@gmail.com>
2026-06-18 17:02:28 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,19 @@
+class Sensor {
+    String name;
+    int batteryLevel;
+
+    void charge(){
+        batteryLevel = 100;
+        System.out.println("Sensor fully changed");
+    }
+}
+
+public class main4 {
+    public static void main(String[] args){
+        Sensor sensor = new Sensor();
+        sensor.charge();
+        System.out.println("Battery Level: " + sensor.batteryLevel);
+    }
+    
+}
 

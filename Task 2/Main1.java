b49eed60a92f9959567f5592eb0b3476fea0321e
SDShuvo07 <ssd01733@gmail.com>
2026-06-18 17:00:30 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,13 @@
+class ContestStates{
+    int[] scores={85,90,95};
+    void printFirstScore(){
+        System.out.println(scores[0]);
+    }
+}
 
+public class main1{
+    public static void main(String[] args){
+        ContestStates cs = new ContestStates();
+        cs.printFirstScore();
+    }
+}
